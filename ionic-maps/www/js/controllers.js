484ae19218358353b7f421b172d6dfc87b6e4af7
flaviobrognara <flavio.brognara@outlook.com>
2016-01-27 13:35:28 +0000

    asds

--- ionic-maps/www/js/controllers.js
+++ ionic-maps/www/js/controllers.js
@@ -1,3 +1,44 @@
+
+angular.module('app.controllers', [])
+   
+
+
+        .controller('loginCtrl', function($scope) {
+        
+        })
+           
+        .controller('rockCtrl', function($scope, $http) {
+            
+        $scope.buscaAPI = function(busca2){
+        // Simple GET request example:
+        // 1U1dAjZnNXXozjtiTyFyFo9po8MXLMWV0aiCUBWZerwIYXMMZu
+        var url = "https://api.tumblr.com/v2/tagged?tag=rock&api_key=1U1dAjZnNXXozjtiTyFyFo9po8MXLMWV0aiCUBWZerwIYXMMZu";
+        var resto_da_url = "?api_key=";
+        $http.get(url).then(function successCallback(response) {
+            //$scope.buscou = true;
+            console.log(response.data);
+            $scope.data = response.data;
+        }, function errorCallback(response) {
+            alert(JSON.stringify(response));
+        });
+       
+        
+        
+        };
+        
+        })
+           
+        .controller('categoriasCtrl', function($scope) {
+        
+        })
+           
+        .controller('signupCtrl', function($scope) {
+        
+        }) 
+
+/*
+
+
 angular.module('app.controllers', [])
   
 .controller('loginCtrl', function($scope) {
@@ -14,5 +55,7 @@ angular.module('app.controllers', [])
    
 .controller('signupCtrl', function($scope) {
 
-})
- 
+}) 
+
+*/
+
