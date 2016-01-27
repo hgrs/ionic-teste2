2da7f73f2248f900327ed8502ccaa30cc7e620c9
flaviobrognara <flavio.brognara@outlook.com>
2016-01-27 18:10:23 +0000

    adsasd

--- ionic-maps/www/js/controllers.js
+++ ionic-maps/www/js/controllers.js
@@ -13,11 +13,15 @@ angular.module('app.controllers', [])
         // Simple GET request example:
         // 1U1dAjZnNXXozjtiTyFyFo9po8MXLMWV0aiCUBWZerwIYXMMZu
         var url = "https://api.foursquare.com/v2/venues/search?query=rock&near=sao%20paulo&oauth_token=YEP244DHKFHU1MDSL55YL20QYE33E3YYUN5MADDSG25MSYG0&v=20160127";
-        var resto_da_url = "?api_key=";
+        
         $http.get(url).then(function successCallback(response) {
             //$scope.buscou = true;
             console.log(response.data);
             $scope.data = response.data.response.venues;
+            
+            $scope.imagem = response.data.response.icon.prefix + response.data.response.icon.suffix; //  junta prefix e sufix da url - cria link da imagem
+            
+            
             
             
               
